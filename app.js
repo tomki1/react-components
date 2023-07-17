d9433da0485d1ce962b5a9d6086ecb9b1d497a7a
Kimberly Tom <kimberlyjtom@gmail.com>
2023-07-17 11:39:50 -0700

    created grocery list

--- app.js
+++ app.js
@@ -1,1 +1,62 @@
-// TODO
+const { useState } = React
+
+// bold when hovering over a list item
+const GroceryListItem = (props) => {
+  const [isBold, setIsBold] = useState(false);
+
+  const style = {
+    fontWeight: isBold ? 'bold' : 'normal',
+  };
+
+  return (
+    <li style={style} onMouseEnter={() => setIsBold(!isBold)} onMouseLeave={() => setIsBold(!isBold)}>
+      {props.groceryItem}
+    </li>
+  );
+  };
+
+const GroceryList = (props) => (
+<ul>
+  {props.GroceryItems.map((groceryItem) => (
+    <GroceryListItem groceryItem={groceryItem}/>
+  ))}
+</ul>
+);
+
+// // strike through when clicked
+// const GroceryListItem = (props) => {
+//   const [inBasket, setInBasket] = useState(false);
+
+//   const style = {
+//     textDecoration: inBasket ? 'line-through' : 'none',
+//   };
+
+//   return (
+//     <li style={style} onClick={() => setInBasket(!inBasket)}>
+//       {props.groceryItem}
+//     </li>
+//   );
+//   };
+
+// const GroceryList = (props) => (
+// <ul>
+//   {props.GroceryItems.map((groceryItem) => (
+//     <GroceryListItem groceryItem={groceryItem}/>
+//   ))}
+// </ul>
+// );
+
+
+const App = () => (
+<div>
+  <h1>Grocery List</h1>
+  <GroceryList GroceryItems={['milk', 'eggs', 'cheese']}/>
+</div>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById(
+    'app'
+  )
+);
